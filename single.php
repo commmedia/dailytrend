06ce1c1c36a80285b73ed9c7425616eafea5fc50
Pablo <pablo@commm.co>
2019-05-27 21:26:06 -0500

    get the content post

--- single.php
+++ single.php
@@ -7,51 +7,65 @@
  */
 get_header(); ?>
 
+<?php if (have_posts()) :
+        ?>
+
+        <?php while (have_posts()) : the_post();
+        ?>
+
+
 <div class="coat">
 
     <div class="block small-12 medium-8">
 
         <div class="post-container">
+
             <div class="post-thumbnail">
                 <img src="<?php the_post_thumbnail_url( 'trend_default' )?>" />
             </div>
+
             <div class="post-title">
                 <h1><?php the_title() ?></h1>
             </div>
+
             <div class="post-meta">
-            <ul class="nav-list">
-                <li class="item"><?php the_time('M d, Y'); ?></li>
-                <li class="item"><?php the_category(); ?></li>
-                <li class="item">Por <?php the_author_posts_link(); ?></li>
-            </ul>
+                <ul class="nav-list">
+                    <li class="item"><?php the_time('M d, Y'); ?></li>
+                    <li class="item"><?php the_category('&bull;'); ?></li>
+                    <li class="item">Por </li>
+                </ul>
             </div>
 
-            <div class="post-excerpt">
-                <?php the_excerpt(); ?>
+           
+
+            <div class="post-content">
+                <?php the_content(); ?>
             </div>
 
-        <div class="post-content">
-             <?php the_content(); ?>
         </div>
-
-        </div>
-
-        
-
     </div>
 
 
     <div class="block small-12 medium-4">
-
+        <div class="pad">
              <?php if (is_active_sidebar('sidebar-1')) : ?>
                 <div id="primary-sidebar" >
                     <?php dynamic_sidebar('sidebar-1'); ?>
                 </div>
             <?php endif; ?>
-
+        </div>
     </div>
 
+
 </div>
+
+<?php endwhile; ?>
+
+        <?php else : ?>
+
+
+
+        <?php endif; ?>
 
 
 
