24d585d40b3591227e5b4dab2a58f2e098393a55
Pablo <pablo@commm.co>
2019-06-04 15:34:57 -0500

    page 404

---
+++ 404.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+
+
+    <div id="content" role="main" class="page">
+        <div class="coat">
+
+
+<div class="coat" style="margin-left:auto;margin-right:auto;text-align:center">
+<div class="block center" style="padding: 23vh;">
+<h1>Error 404 </h1>
+<p>Oooops. La página no se ha encontrado.</p>
+<a class="button" href="/">Vuelve al inicio</a>
+</div>
+</div>
+
+
+
+        
+
+        </div>
+    </div>
+    <?php get_footer(); ?>
