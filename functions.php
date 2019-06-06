ab18ace51e8c0af40de87668b78f91ac697f9577
Pablo <pablo@commm.co>
2019-06-05 20:09:08 -0500

    Popular post count

--- functions.php
+++ functions.php
@@ -290,7 +290,28 @@ add_shortcode('subhero', 'trend_posts_banner_subhero');
 
 
 
-// 7. Acf groups
+
+
+// 7. Custom post functions
+// ==========================================================================
+
+function count_post_visits() {
+ if( is_single() ) {
+ global $post;
+ $views = get_post_meta( $post->ID, 'my_post_viewed', true );
+ if( $views == '' ) {
+ update_post_meta( $post->ID, 'my_post_viewed', '1' ); 
+ } else {
+ $views_no = intval( $views );
+ update_post_meta( $post->ID, 'my_post_viewed', ++$views_no );
+ }
+ }
+}
+add_action( 'wp_head', 'count_post_visits' );
+
+
+
+// 8. Acf groups
 // ==========================================================================
 
 
