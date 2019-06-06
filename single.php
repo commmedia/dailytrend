337f1440fb6e02961b0ab6c7fec0a81a1392bf15
Pablo <pablo@commm.co>
2019-06-06 13:06:57 -0500

    Delete via trends comment box

--- single.php
+++ single.php
@@ -44,7 +44,6 @@ get_header(); ?>
                 <form class="block" action="https://twitter.com/intent/tweet" name="ComnentBox" target="_blank">
                     <div class="coat inputer">
                         <textarea id="ComnentBoxText" name="text" placeholder="Mensaje..."></textarea>
-                        <input type="hidden" value="trendbusinessco" name="via" />
                         <input type="hidden" value="<?php the_permalink() ?>" name="url" />
                         <input type="submit" class="button block small-12 medium-4" value="Twittear" />
                     </div>
