cf2cfc1db76f277c898275fd06c23b33f7845d9a
Pablo <pablo@commm.co>
2019-06-19 09:54:47 -0500

    Style login comm

--- functions.php
+++ functions.php
@@ -445,8 +445,32 @@ if(!function_exists('video_content_filter')) {
    }
 
 
+// 9. Custom Login
+// ==========================================================================
+function my_login_logo_url()
+{
+    return home_url();
+}
+add_filter('login_headerurl', 'my_login_logo_url');
+function my_login_logo_url_title()
+{
+    return 'Nombre del sitio web';
+}
+add_filter('login_headertitle', 'my_login_logo_url_title');
+function my_login_logo()
+{ ?>
+    
+<?php 
+}
+add_action('login_enqueue_scripts', 'my_login_logo');
+function my_login_stylesheet()
+{
+    wp_enqueue_style('custom-login', get_stylesheet_directory_uri() . '/css/style-login.css');
+}
+add_action('login_enqueue_scripts', 'my_login_stylesheet');
 
-// 9. Acf groups
+
+// 10. Acf groups
 // ==========================================================================
 if( function_exists('acf_add_local_field_group') ):
 
